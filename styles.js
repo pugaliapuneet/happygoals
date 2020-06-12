b4a1116290ba9cf751f4a6d7ee0e2a5b8ef89821
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-06-12 12:11:32 +0530

    slight design change

--- styles.js
+++ styles.js
@@ -94,7 +94,7 @@ const styles = StyleSheet.create({
       elevation: 5
     },
 
-  journalScore: {fontSize: 50, lineHeight: 60, color: primaryColor, fontFamily: 'Nunito-ExtraLight', textAlign: 'center'},
+  journalScore: {fontSize: 40, lineHeight: 60, color: primaryColor, fontFamily: 'Nunito-Regular', textAlign: 'center'},
 });
 
 
